c425fab98b5fed6ffa3c8f74afdd583e31f0854f
Mike Hawk <cattyreal5541@gmail.com>
2026-03-05 18:50:41 +0000

    Remove build cache and add gitignore

--- build/CMakeFiles/NEATPlayer_PACKAGE.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/NEATPlayer_PACKAGE"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/NEATPlayer_PACKAGE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
